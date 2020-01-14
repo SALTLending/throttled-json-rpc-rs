52b1d74b0cec406e09ce939772921fca7ec65c90
Justin Miller <justin.miller@saltlending.com>
2020-01-14 11:43:55 -0700

    feat: Remove the arc constraint

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ use std::sync::Arc;
 
 pub struct ReqBatcher {
     ask_request: futures::channel::mpsc::Sender<RequestCallbackPair>,
-    client_options: ClientOptions,
 }
 
 #[derive(Debug, Clone)]
@@ -41,30 +40,24 @@ impl RPS {
 }
 
 impl ReqBatcher {
-    pub fn new(client_options: ClientOptions) -> Arc<Self> {
+    pub fn new(client_options: ClientOptions) -> Self {
         let (ask_request, requests) = futures::channel::mpsc::channel(10_000);
         let chunk_size = client_options.batching;
         let concurrent_count = client_options.concurrent;
-        let req_batcher = Arc::new(ReqBatcher {
-            ask_request,
-            client_options,
-        });
-        let answer = req_batcher.clone();
+        let request_batcher = ReqBatcher { ask_request };
+        let client_options = Arc::new(client_options);
         tokio::spawn(async move {
             requests
                 .chunks(chunk_size)
-                .map(|x| (req_batcher.clone(), x))
-                .for_each_concurrent(concurrent_count, |(req_batcher, xs)| {
-                    async move {
-                        req_batcher
-                            .request_json_rpc(xs)
-                            .await
-                            .unwrap_or_else(|e| log::error!("Error in client: {:?}", e))
-                    }
+                .map(|x| (client_options.clone(), x))
+                .for_each_concurrent(concurrent_count, |(client_options, xs)| async move {
+                    request_json_rpc(&client_options, xs)
+                        .await
+                        .unwrap_or_else(|e| log::error!("Error in client: {:?}", e))
                 })
                 .await;
         });
-        answer
+        request_batcher
     }
 
     pub async fn request<'a, T>(&mut self, method: String, params: Vec<Value>) -> Result<T>
@@ -77,79 +70,6 @@ impl ReqBatcher {
         let value = serde_json::from_value(received_value?)
             .map_err(|_| anyhow!("Could not convert {:?} into type"))?;
         Ok(value)
-    }
-
-    async fn request_json_rpc(&self, chunk: Vec<RequestCallbackPair>) -> Result<()> {
-        use serde_json::json;
-        let client = reqwest::Client::new();
-        let client_options = &self.client_options;
-        let mut builder = client.post(&client_options.uri);
-        let is_not_batching = client_options.batching <= 1;
-        if let Some(client_auth) = &client_options.client_auth {
-            builder = builder.basic_auth(client_auth.user.clone(), client_auth.password.clone());
-        }
-        let data: Vec<Value> = chunk
-            .iter()
-            .enumerate()
-            .map(|(i, (method, params, ..))| {
-                json!({
-                    "method": method,
-                    "params": values_cleanse(params.clone()),
-                    "id": i.to_string(),
-                })
-            })
-            .collect();
-        if chunk.is_empty() {
-            return Ok(());
-        }
-        builder = if data.len() == 1 && is_not_batching {
-            builder.json(&data[0])
-        } else {
-            builder.json(&data)
-        };
-        let res = builder.send();
-        if let RPS(RpsState::Limit(rps)) = client_options.rps {
-            let wait = if rps > 1.0 {
-                std::time::Duration::from_secs(1) / rps as u32
-            } else {
-                std::time::Duration::from_secs(1) * (1.0 / rps) as u32
-            };
-            tokio::time::delay_for(wait).await;
-        }
-        let response: Value = res
-            .and_then(|x| x.json())
-            .await
-            .with_context(|| anyhow!("Calling rpc with data {:?}", chunk))?;
-        let response: Vec<_> = match response {
-            Value::Array(values) => values,
-            x if is_not_batching => vec![x],
-            x => bail!(
-                "Expecting during batch that results come back as array: {:?}",
-                x
-            ),
-        };
-        for (raw_response, (method, requests, respond)) in
-            response.into_iter().zip(chunk.into_iter())
-        {
-            use serde_json::from_value;
-            let response = match from_value(raw_response) {
-                Ok(JsonResponse::Result { result: x }) => Ok(x),
-                Ok(JsonResponse::Error { error }) => Err(anyhow!(
-                    "Error response from json rpc {:?} given {:?}",
-                    error,
-                    (method, requests)
-                )),
-                x => Err(anyhow!(
-                    "Couldn't parse json rpc response of {:?} given {:?}",
-                    x,
-                    (method, requests)
-                )),
-            };
-            if let Err(x) = respond.send(response) {
-                bail!("Failed responding for oneshot for value: {:?}", x);
-            }
-        }
-        Ok(())
     }
 }
 
@@ -185,4 +105,77 @@ fn values_cleanse(values: Vec<Value>) -> Vec<Value> {
         .into_iter()
         .rev()
         .collect::<Vec<Value>>()
+}
+
+async fn request_json_rpc(
+    client_options: &ClientOptions,
+    chunk: Vec<RequestCallbackPair>,
+) -> Result<()> {
+    use serde_json::json;
+    let client = reqwest::Client::new();
+    let mut builder = client.post(&client_options.uri);
+    let is_not_batching = client_options.batching <= 1;
+    if let Some(client_auth) = &client_options.client_auth {
+        builder = builder.basic_auth(client_auth.user.clone(), client_auth.password.clone());
+    }
+    let data: Vec<Value> = chunk
+        .iter()
+        .enumerate()
+        .map(|(i, (method, params, ..))| {
+            json!({
+                "method": method,
+                "params": values_cleanse(params.clone()),
+                "id": i.to_string(),
+            })
+        })
+        .collect();
+    if chunk.is_empty() {
+        return Ok(());
+    }
+    builder = if data.len() == 1 && is_not_batching {
+        builder.json(&data[0])
+    } else {
+        builder.json(&data)
+    };
+    let res = builder.send();
+    if let RPS(RpsState::Limit(rps)) = client_options.rps {
+        let wait = if rps > 1.0 {
+            std::time::Duration::from_secs(1) / rps as u32
+        } else {
+            std::time::Duration::from_secs(1) * (1.0 / rps) as u32
+        };
+        tokio::time::delay_for(wait).await;
+    }
+    let response: Value = res
+        .and_then(|x| x.json())
+        .await
+        .with_context(|| anyhow!("Calling rpc with data {:?}", chunk))?;
+    let response: Vec<_> = match response {
+        Value::Array(values) => values,
+        x if is_not_batching => vec![x],
+        x => bail!(
+            "Expecting during batch that results come back as array: {:?}",
+            x
+        ),
+    };
+    for (raw_response, (method, requests, respond)) in response.into_iter().zip(chunk.into_iter()) {
+        use serde_json::from_value;
+        let response = match from_value(raw_response) {
+            Ok(JsonResponse::Result { result: x }) => Ok(x),
+            Ok(JsonResponse::Error { error }) => Err(anyhow!(
+                "Error response from json rpc {:?} given {:?}",
+                error,
+                (method, requests)
+            )),
+            x => Err(anyhow!(
+                "Couldn't parse json rpc response of {:?} given {:?}",
+                x,
+                (method, requests)
+            )),
+        };
+        if let Err(x) = respond.send(response) {
+            bail!("Failed responding for oneshot for value: {:?}", x);
+        }
+    }
+    Ok(())
 }
