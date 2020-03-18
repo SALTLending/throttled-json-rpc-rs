d9d6fbbd9d92c518f69a6c7d976b9197307bcf00
Justin Miller <justin.miller@saltlending.com>
2020-03-18 12:33:36 -0600

    chore: Add tests and docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
-#![deny(missing_docs, missing_debug_implementations)]
+#![deny(missing_docs, missing_debug_implementations, missing_doc_code_examples)]
 
 //! Request batcher is a request client for anything that is json rpc [1].
 //! There are quite a few limiters on this client, like the RPS, max batching, and even how many
 //! allowed concurrently
 
-use anyhow::{anyhow, bail, Context, Result};
+use anyhow::{anyhow, Context, Result};
 use futures::channel::oneshot;
 use futures::prelude::*;
 use reqwest;
+use reqwest::Client;
 use serde_json::Value;
 use std::sync::Arc;
 
@@ -52,11 +53,19 @@ pub struct ClientAuth {
 /// We want the enum behind a new type because we need to ensure that there
 /// isn't a limit with 0.0 for the value. And since we are using a float we can't
 /// ensure that the value is postive.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct RPS(RpsState);
 
 impl RPS {
     /// Create a RPS with a limit being a value > 0, and as a float.
+    ///
+    /// ```
+    /// # use throttled_json_rpc::*;
+    /// assert_eq!("RPS(Limit(1.4))", &format!("{:?}", RPS::new(1.4).unwrap()),);
+    /// assert_eq!("RPS(None)", &format!("{:?}", RPS::new(0.0).unwrap()),);
+    /// RPS::new(-1.0).expect_err("Should error for negative");
+    /// assert_eq!(RPS::none(), RPS::new(0.0).unwrap());
+    /// ```
     pub fn new(value: f64) -> Result<Self> {
         if value < 0.0 {
             return Err(anyhow::anyhow!("Invalid RPS value: {} <= 0", value));
@@ -67,6 +76,10 @@ impl RPS {
         Ok(RPS(RpsState::Limit(value)))
     }
     /// Create a rps that indicates there are no limits based on requests per second
+    /// ```
+    /// # use throttled_json_rpc::*;
+    /// assert_eq!("RPS(None)", &format!("{:?}", RPS::none()));
+    /// ```
     pub fn none() -> Self {
         RPS(RpsState::None)
     }
@@ -83,15 +96,17 @@ impl ReqBatcher {
         };
         let request_batcher = ReqBatcher { ask_request };
         let client_options = Arc::new(client_options);
+        let client = reqwest::Client::new();
         tokio::spawn(async move {
             requests
                 .chunks(chunk_size)
-                .map(|x| (client_options.clone(), x))
-                .for_each_concurrent(concurrent_count, |(client_options, xs)| async move {
-                    request_json_rpc(&client_options, xs)
-                        .await
-                        .unwrap_or_else(|e| log::error!("Error in client: {:?}", e))
-                })
+                .map(|x| (client_options.clone(), client.clone(), x))
+                .for_each_concurrent(
+                    concurrent_count,
+                    |(client_options, client, xs)| async move {
+                        request_json_rpc(&client_options, xs, client).await
+                    },
+                )
                 .await;
         });
         request_batcher
@@ -122,7 +137,7 @@ enum JsonResponse {
     Error { error: Value },
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 enum RpsState {
     None,
     Limit(f64),
@@ -150,13 +165,16 @@ fn values_cleanse(values: Vec<Value>) -> Vec<Value> {
 async fn request_json_rpc(
     client_options: &ClientOptions,
     chunk: Vec<RequestCallbackPair>,
-) -> Result<()> {
+    client: Client,
+) {
     use serde_json::json;
-    let client = reqwest::Client::new();
     let mut builder = client.post(&client_options.uri);
     let is_not_batching = client_options.batching <= 1;
     if let Some(client_auth) = &client_options.client_auth {
         builder = builder.basic_auth(client_auth.user.clone(), client_auth.password.clone());
+    }
+    if chunk.is_empty() {
+        return ();
     }
     let data: Vec<Value> = chunk
         .iter()
@@ -169,9 +187,6 @@ async fn request_json_rpc(
             })
         })
         .collect();
-    if chunk.is_empty() {
-        return Ok(());
-    }
     builder = if data.len() == 1 && is_not_batching {
         builder.json(&data[0])
     } else {
@@ -186,36 +201,224 @@ async fn request_json_rpc(
         };
         tokio::time::delay_for(wait).await;
     }
-    let response: Value = res
-        .and_then(|x| x.json())
-        .await
-        .with_context(|| anyhow!("Calling rpc with data {:?}", chunk))?;
-    let response: Vec<_> = match response {
-        Value::Array(values) => values,
-        x if is_not_batching => vec![x],
-        x => bail!(
+    let response: Result<Value> = res.and_then(|x| x.json()).await.with_context(|| {
+        anyhow!(
+            "Calling rpc with data {}",
+            serde_json::to_string(&data)
+                .unwrap_or_else(|_| format!("Could not decode ({:?})", data))
+        )
+    });
+
+    let response: Result<Vec<_>> = match response {
+        Err(e) => Err(e),
+        Ok(Value::Array(values)) => {
+            if values.len() == chunk.len() {
+                Ok(values)
+            } else {
+                Err(anyhow!("Response length {} does not match callback length {} for vallback values of {:?}", values.len(), chunk.len(), values))
+            }
+        }
+        Ok(x) if is_not_batching => Ok(vec![x]),
+        Ok(x) => Err(anyhow!(
             "Expecting during batch that results come back as array: {:?}",
             x
-        ),
+        )),
     };
-    for (raw_response, (method, requests, respond)) in response.into_iter().zip(chunk.into_iter()) {
-        use serde_json::from_value;
-        let response = match from_value(raw_response) {
-            Ok(JsonResponse::Result { result: x }) => Ok(x),
-            Ok(JsonResponse::Error { error }) => Err(anyhow!(
-                "Error response from json rpc {:?} given {:?}",
-                error,
-                (method, requests)
-            )),
-            x => Err(anyhow!(
-                "Couldn't parse json rpc response of {:?} given {:?}",
-                x,
-                (method, requests)
-            )),
-        };
-        if let Err(x) = respond.send(response) {
-            bail!("Failed responding for oneshot for value: {:?}", x);
+    match response {
+        Ok(response) => {
+            for (raw_response, (method, requests, respond)) in
+                response.into_iter().zip(chunk.into_iter())
+            {
+                use serde_json::from_value;
+                let response = match from_value(raw_response) {
+                    Ok(JsonResponse::Result { result: x }) => Ok(x),
+                    Ok(JsonResponse::Error { error }) => Err(anyhow!(
+                        "Error response from json rpc {:?} given {:?}",
+                        error,
+                        (method, requests)
+                    )),
+                    x => Err(anyhow!(
+                        "Couldn't parse json rpc response of {:?} given {:?}",
+                        x,
+                        (method, requests)
+                    )),
+                };
+                if let Err(x) = respond.send(response) {
+                    log::error!("Failed responding for oneshot for value: {:?}", x);
+                }
+            }
+        }
+        Err(e) => {
+            for (_method, _requests, respond) in chunk.into_iter() {
+                let error = Err(anyhow!("{:?}", e));
+                if let Err(x) = respond.send(error) {
+                    log::error!(
+                        "Failed responding with error for oneshot for value: {:?}",
+                        x
+                    );
+                }
+            }
         }
     }
-    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::{server_address, Matcher};
+    use tokio_test::block_on;
+
+    #[test]
+    fn test_limited_requests_rps() {
+        let mock = mockito::mock("POST", Matcher::Any)
+            .with_body(r#"[{"result": "test"}]"#)
+            .expect(2)
+            .create();
+        block_on(async {
+            let mut delay = tokio::time::delay_for(tokio::time::Duration::from_millis(1100)).fuse();
+            let requester = ReqBatcher::new(ClientOptions {
+                uri: format!("http://{}", server_address()),
+                batching: 1,
+                client_auth: None,
+                concurrent: 1,
+                rps: RPS::new(2.0).unwrap(),
+            });
+            let requests: Vec<_> = (0..10)
+                .map(|_i| requester.clone())
+                .map(|mut requester| async move {
+                    requester
+                        .request::<String>("test".to_string(), vec![])
+                        .await
+                        .expect("testing request");
+                })
+                .collect();
+            let mut requests = futures::future::join_all(requests).fuse();
+
+            futures::select! {
+                _ = delay => {
+                }
+                requests = requests => {
+                    panic!("Not expecting that all the requests finnish, because it should be throttled");
+                }
+            }
+        });
+        mock.assert()
+    }
+    #[test]
+    fn test_limited_requests_rps_concurrent() {
+        let mock = mockito::mock("POST", Matcher::Any)
+            .with_body(r#"[{"result": "test"}]"#)
+            .expect(8)
+            .create();
+        block_on(async {
+            let mut delay = tokio::time::delay_for(tokio::time::Duration::from_millis(2100)).fuse();
+
+            let requester = ReqBatcher::new(ClientOptions {
+                uri: format!("http://{}", server_address()),
+                batching: 1,
+                client_auth: None,
+                concurrent: 2,
+                rps: RPS::new(2.0).unwrap(),
+            });
+            let requests: Vec<_> = (0..10)
+                .map(|_i| requester.clone())
+                .map(|mut requester| async move {
+                    requester
+                        .request::<String>("test".to_string(), vec![])
+                        .await
+                        .expect("testing request");
+                })
+                .collect();
+            let mut requests = futures::future::join_all(requests).fuse();
+
+            futures::select! {
+                _ = delay => {
+                }
+                requests = requests => {
+                    panic!("Not expecting that all the requests finnish, because it should be throttled");
+                }
+            }
+        });
+        mock.assert();
+    }
+
+    #[test]
+    #[should_panic(
+        expected = r#"testing request: Response length 1 does not match callback length 2 for vallback values of [Object({"result": String("test")})]"#
+    )]
+    fn test_limited_requests_rps_batched_failure() {
+        let mock = mockito::mock("POST", Matcher::Any)
+            .with_body(r#"[{"result": "test"}]"#)
+            .expect(2)
+            .create();
+        block_on(async {
+            let mut delay = tokio::time::delay_for(tokio::time::Duration::from_millis(1100)).fuse();
+
+            let requester = ReqBatcher::new(ClientOptions {
+                uri: format!("http://{}", server_address()),
+                batching: 2,
+                client_auth: None,
+                concurrent: 1,
+                rps: RPS::new(2.0).unwrap(),
+            });
+            let requests: Vec<_> = (0..10)
+                .map(|_i| requester.clone())
+                .map(|mut requester| async move {
+                    requester
+                        .request::<String>("test".to_string(), vec![])
+                        .await
+                        .expect("testing request");
+                })
+                .collect();
+            let mut requests = futures::future::join_all(requests).fuse();
+
+            futures::select! {
+                _ = delay => {
+                }
+                requests = requests => {
+                    panic!("Not expecting that all the requests finnish, because it should be throttled");
+                }
+            }
+        });
+        mock.assert()
+    }
+
+    #[test]
+    fn test_limited_requests_rps_batched() {
+        let mock2 = mockito::mock("POST", Matcher::Any)
+        .match_body(r#"[{"id":"0","method":"test","params":[]},{"id":"1","method":"test","params":[]}]"#)
+            .with_body(r#"[{"result": "test"},{"result": "test2"}]"#)
+            .expect(2)
+            .create();
+
+        block_on(async {
+            let mut delay = tokio::time::delay_for(tokio::time::Duration::from_millis(1100)).fuse();
+            let requester = ReqBatcher::new(ClientOptions {
+                uri: format!("http://{}", server_address()),
+                batching: 2,
+                client_auth: None,
+                concurrent: 1,
+                rps: RPS::new(2.0).unwrap(),
+            });
+            let requests: Vec<_> = (0..10)
+                .map(|_i| requester.clone())
+                .map(|mut requester| async move {
+                    requester
+                        .request::<String>("test".to_string(), vec![])
+                        .await
+                        .expect("testing request")
+                })
+                .collect();
+            let mut requests = futures::future::join_all(requests).fuse();
+
+            futures::select! {
+                _ = delay => {
+                }
+                requests = requests => {
+                    panic!("Not expecting that all the requests finnish, because it should be throttled: {:?}", requests);
+                }
+            }
+        });
+        mock2.assert();
+    }
 }
